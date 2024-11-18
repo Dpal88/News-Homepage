c4a8d2542ba6d76067d587d2e8edd2b59f6966b6
Dominic Palomino <MynameisDomPal@gmail.com>
2024-11-18 15:41:33 -0600

    changed class names to match BEM. Also added code comments in CSSfile

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 const container = document.querySelector('.container');
 
-const hamButton = document.querySelector('.topnav__btn');
+const hamButton = document.querySelector('.header__btn');
 
-const topNavMenu = document.querySelector('.topnav__menu');
-const opacity = document.querySelector('.opacity');
+const topNavMenu = document.querySelector('.header__menu');
+const opacityDiv = document.querySelector('.opacity-BG');
 
 hamButton.addEventListener('click', e => {
-    const closeIcon = document.querySelector('.close-icon');
-    const hamIcon = document.querySelector('.ham-icon');
+    const closeIcon = document.querySelector('.header__icon--close');
+    const hamIcon = document.querySelector('.header__icon--ham');
 
     hamIcon.classList.toggle('hide');
     closeIcon.classList.toggle('hide');
 
     topNavMenu.classList.toggle('active');
-    opacity.classList.toggle('activeBG');
+    opacityDiv.classList.toggle('activeBG');
 })
